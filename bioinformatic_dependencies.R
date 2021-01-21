0da32de68ccbf71f53c4e93c0703ce5733b695b2
brunoconde07 <brunoconde.ufpa@gmail.com>
2021-01-21 13:36:41 -0300

    (bioinformatic_dependencies.R, at /) -> initial bioinformatic dependencies, with only tximport code instalation

---
+++ bioinformatic_dependencies.R
@@ -0,0 +1,20 @@
+# library(tidyverse)
+# library(tximport)
+# library(EnsDb.Hsapiens.v86)
+# library(DESeq2)
+
+# install.packages(pkgs="tximport", lib ="/usr/lib/R/library", dependencies=T) # Erro
+install.packages(pkgs="EnsDb.Hsapiens.v86", lib ="/usr/lib/R/library", dependencies=T) # Erro
+install.packages(pkgs="DESeq2", lib ="/usr/lib/R/library", dependencies=T) # Erro
+
+###
+# tximport
+# https://bioconductor.org/packages/devel/bioc/html/tximport.html
+###
+if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+
+BiocManager::install("tximport")
+
+# to see documentation
+# browseVignettes("tximport")
