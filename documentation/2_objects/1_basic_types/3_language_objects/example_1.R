090fec08f07edad80ad2b6f0f2082777face1c3b
brunoconde07 <brunoconde.ufpa@gmail.com>
2021-01-22 03:24:56 -0300

    (2_objects/1_basic_types/3_language_objects/example_1.R, at documentation/) -> calls, expressions and names are the language objects

--- documentation/2_objects/1_basic_types/3_language_objects/example_1.R
+++ documentation/2_objects/1_basic_types/3_language_objects/example_1.R
@@ -1,5 +1,5 @@
-There are three types of objects that constitute the R language. They are calls, expressions, and names. Since R has objects of type "expression" we will try to avoid the use of the word expression in other contexts. In particular syntactically correct expressions will be referred to as statements.
+# There are three types of objects that constitute the R language. They are calls, expressions, and names. Since R has objects of type "expression" we will try to avoid the use of the word expression in other contexts. In particular syntactically correct expressions will be referred to as statements.
 
-These objects have modes "call", "expression", and "name", respectively.
+# These objects have modes "call", "expression", and "name", respectively.
 
-They can be created directly from expressions using the quote mechanism and converted to and from lists by the as.list and as.call functions. Components of the parse tree can be extracted using the standard indexing operations.
+# They can be created directly from expressions using the quote mechanism and converted to and from lists by the as.list and as.call functions. Components of the parse tree can be extracted using the standard indexing operations.
