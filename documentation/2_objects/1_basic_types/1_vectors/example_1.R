d01b5f14dd9ee427e57008f474e28887625bb4c3
brunoconde07 <brunoconde.ufpa@gmail.com>
2021-01-22 03:13:13 -0300

    (2_objects/1_basic_types/1_vectors, at doumentation/) -> all is vectors with lenth at least one and, when useful, its possible to use vectors with length 0

---
+++ documentation/2_objects/1_basic_types/1_vectors/example_1.R
@@ -0,0 +1,13 @@
+# R has six basic (‘atomic’) vector types: logical, integer, real, complex, string (or character) and raw. The modes and storage modes for the different vector types are listed in the following table.
+
+# typeof	mode	storage.mode
+# logical	logical	logical
+# integer	numeric	integer
+# double	numeric	double
+# complex	complex	complex
+# character	character	character
+# raw	raw	raw
+
+# Single numbers, such as 4.2, and strings, such as "four point two" are still vectors, of length 1; there are no more basic types. Vectors with length zero are possible (and useful).
+
+# String vectors have mode and storage mode "character". A single element of a character vector is often referred to as a character string.
