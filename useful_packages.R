f9ed17678d5b56fb7b7bb35bdcd40b6755dec5d0
brunoconde07 <brunoconde.ufpa@gmail.com>
2021-01-24 01:28:00 -0300

    (useful_packages.R, at /) -> initial commit. 'here' package, to use with source command

---
+++ useful_packages.R
@@ -0,0 +1,7 @@
+# -> here: to use with the source command
+# https://stackoverflow.com/questions/42815889/r-source-and-path-to-source-files
+
+install.packages(
+	pkgs="here", 
+	dependencies=TRUE,
+	lib ="/usr/lib/R/library")
